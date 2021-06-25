f524b68d447e960de06cfb3d31ede9cae4c5d442
GMargherini <84579545+GMargherini@users.noreply.github.com>
2021-06-25 16:23:21 +0200

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -9,24 +9,28 @@ public class FileManager {
 	String separator;
 	
 	public FileManager(String fileName){ //fileName: nome del file a cui accedere senza estensione
-		this.fileName=fileName;
+		this.fileName=fileName+".dati";
+		this.path=getPath()+this.fileName;
+		this.file=getFile(this.fileName);
+	}
+	public FileManager(){
 		this.path=getPath();
-		this.file=getFile();
+		this.file=getFile("");
 	}
 	
 	private String getPath(){
 		separator=File.separator;
-		return ".."+separator+"data"+separator+fileName+".dati";
+		return ".."+separator+"data"+separator;
 	}
 	
-	private File getFile(){
+	private File getFile(String name){
 		File f=new File(path);
 		if(!f.exists()){
-			System.out.print("File non trovato, creare "+fileName+".dati ? (s/n) ");
+			System.out.print("File non trovato, creare "+name+" ? (s/n) ");
 			Scanner sc=new Scanner(System.in);
 			String ans=sc.nextLine().toLowerCase();
 			while(!ans.equals("s")&&!ans.equals("n")){
-				System.out.print("Input non valido, creare "+fileName+".dati ? (s/n) ");
+				System.out.print("Input non valido, creare "+name+" ? (s/n) ");
 				ans=sc.next().toLowerCase();
 			}
 			if(ans.equals("s")){
@@ -50,6 +54,42 @@ public class FileManager {
 			System.out.print(e);
 		}
 		return f;
+	}
+	
+	public String[] getFileList(){ //Restituisce un vettore con i nomi di tutti i file nella cartella
+		String[] files=file.list();
+		return files;
+	}
+	public void selectFile(){
+		System.out.print("Quale file selezionare? ");
+		Scanner sc=new Scanner(System.in);
+		String ans=sc.nextLine();
+		path=getPath()+ans;
+		file=new File(path);
+		if (file.exists()){
+			deleteFile();
+		}
+		else{
+			file=getFile(ans);
+		}
+	}
+	public void deleteFile(){
+		if(file.exists()){
+			System.out.print("Eliminare "+file.getName()+" ? (s/n) ");
+			Scanner sc=new Scanner(System.in);
+			String ans=sc.nextLine().toLowerCase();
+			while(!ans.equals("s")&&!ans.equals("n")){
+				System.out.print("Input non valido, eliminare "+file.getName()+" ? (s/n) ");
+				ans=sc.next().toLowerCase();
+			}
+			if(ans.equals("s")){
+				file.delete();
+				System.out.print("File eliminato\n");
+			}
+			else if(ans.equals("n")){
+				System.out.print("File non eliminato\n");
+			}
+		}
 	}
 	
 	public String[] read(int line){ //line: riga del file da leggere
