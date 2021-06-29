ca770862bd083cf4a71a057d2efcf2971fb8c38d
GMargherini <84579545+GMargherini@users.noreply.github.com>
2021-06-29 16:04:18 +0200

    Add files via upload

---
+++ DataManager.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileManager;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author dolphin
+ */
+public class DataManager {
+	File file;
+	public DataManager(FileManager fm){
+		file=fm.file;
+	}
+	
+	private String[] split(String str){
+		String[] data=new String[6];
+		String[] split;
+		int i;
+		StringTokenizer st=new StringTokenizer(str,",");
+		for(i=0;st.hasMoreTokens();i++){
+			data[i]=st.nextToken();
+		}
+		split=new String[i];
+		System.arraycopy(data, 0, split, 0, i);
+		return split;
+	}
+	
+	public String[] read(int line){ //line: riga del file da leggere
+		String row;
+		try{
+			String[] data=null;
+			int i=0;
+			BufferedReader br=new BufferedReader(new FileReader(file));
+			if(line==0){
+				row=br.readLine();
+				data=split(row);
+				br.close();
+				return data;
+			}
+			while (( br.readLine()) != null && i<line-1) {
+				i++;
+			}
+			row=br.readLine();
+			data=split(row);
+			br.close();
+			return data;
+		}
+		catch(IOException e1){
+			System.out.print(e1);
+		}
+		catch(NullPointerException e1){
+			System.out.print("Fine file superata\n");
+		}
+		return null;
+	}
+
+	public void write(String[] input){ //input: dati da scrivere su file
+		int len=input.length;
+		try{
+			BufferedWriter bw=new BufferedWriter(new FileWriter(file,true));
+			for(int i=0;i<len-1;i++){
+				bw.write(input[i]+",");
+			}
+			bw.write(input[len-1]+"\n");
+			bw.close();
+		}
+		catch(IOException e){
+			System.out.print(e);
+		}
+		catch(NullPointerException e1){
+			System.out.print("File non trovato\n");
+		}
+	}
+	
+	public int getLineCount(){
+		int i=0;
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while ((br.readLine()) != null){
+				i++;
+			}
+		}
+		catch(IOException e){
+			System.out.print(e);
+		}
+		catch(NullPointerException e1){
+			System.out.print("File non trovato\n");
+		}
+		return i;
+	}
+	
+	public String lastID(){
+		String[] row=read(getLineCount()-1);
+		return row[0];
+	}
+}
--- FileManager.java
+++ FileManager.java
@@ -1,5 +1,6 @@
+package FileManager;
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class FileManager {
@@ -9,20 +10,23 @@ public class FileManager {
 	String separator;
 	
 	public FileManager(String fileName){ //fileName: nome del file a cui accedere senza estensione
-		this.fileName=fileName+".dati";
-		this.path=getPath()+this.fileName;
+		this.fileName=fileName;
+		this.path=getFilePath(this.fileName);
 		this.file=getFile(this.fileName);
 	}
 	public FileManager(){
-		this.path=getPath();
+		this.path=getDirectoryPath();
 		this.file=getFile("");
 	}
 	
-	private String getPath(){
+	private String getDirectoryPath(){
 		separator=File.separator;
 		return ".."+separator+"data"+separator;
 	}
-	
+	private String getFilePath(String name){
+		separator=File.separator;
+		return ".."+separator+"data"+separator+name+".dati";
+	}
 	private File getFile(String name){
 		File f=new File(path);
 		if(!f.exists()){
@@ -34,7 +38,7 @@ public class FileManager {
 				ans=sc.next().toLowerCase();
 			}
 			if(ans.equals("s")){
-				f=newFile();
+				f=newFile(name);
 				System.out.print("File creato\n");
 			}
 			else if(ans.equals("n")){
@@ -45,24 +49,35 @@ public class FileManager {
 		return f;
 	}
 	
-	public File newFile(){
-		File f=new File(path);
-		try{
-			f.createNewFile();
-		}
-		catch(IOException e){
-			System.out.print(e);
+//	private File newFile(){
+//		File f=new File(path);
+//		try{
+//			f.createNewFile();
+//		}
+//		catch(IOException e){
+//			System.out.print(e);
+//		}
+//		return f;
+//	}
+	public File newFile(String name){
+		File f=new File(getFilePath(name));
+		if(!f.exists()){
+			try{
+				f.createNewFile();
+			}
+			catch(IOException e){
+				System.out.print(e);
+			}
 		}
 		return f;
 	}
-	
 	public void deleteFile(){
 		if(file.exists()){
-			System.out.print("Eliminare "+file.getName()+" ? (s/n) ");
+			System.out.print("Eliminare "+fileName+" ? (s/n) ");
 			Scanner sc=new Scanner(System.in);
 			String ans=sc.nextLine().toLowerCase();
 			while(!ans.equals("s")&&!ans.equals("n")){
-				System.out.print("Input non valido, eliminare "+file.getName()+" ? (s/n) ");
+				System.out.print("Input non valido, eliminare "+fileName+" ? (s/n) ");
 				ans=sc.next().toLowerCase();
 			}
 			if(ans.equals("s")){
@@ -75,15 +90,15 @@ public class FileManager {
 		}
 	}
 	
-	public String[] getFileList(){ //Restituisce un vettore con i nomi di tutti i file nella cartella
-		return file.list();
+	public String[] getFileList(){ //Restituisce un vettore con i nomi di tutti file nella cartella
+		String[] files=file.list();
+		return files;
 	}
-	
 	public void selectFile(){
 		System.out.print("Quale file selezionare? ");
 		Scanner sc=new Scanner(System.in);
 		String ans=sc.nextLine();
-		path=getPath()+ans;
+		path=getFilePath(ans);
 		file=new File(path);
 		if (file.exists()){
 			deleteFile();
@@ -93,63 +108,6 @@ public class FileManager {
 		}
 	}
 	
-	public String[] read(int line){ //line: riga del file da leggere
-		String row;
-		try{
-			String[] data=null;
-			int i=0;
-			BufferedReader br=new BufferedReader(new FileReader(file));
-			while (( row = br.readLine()) != null) {
-				if(i==line){
-					data = row.split(",");
-					break;
-				}
-				i++;
-			}
-			br.close();
-			return data;
-		}
-		catch(IOException e1){
-			System.out.print(e1);
-		}
-		catch(NullPointerException e1){
-			System.out.print("File non trovato\n");
-		}
-		return null;
-	}
 	
-	public void write(String[] input){ //input: dati da scrivere su file
-		try{
-			BufferedWriter bw=new BufferedWriter(new FileWriter(file,true));
-			for(int i=0;i<input.length;i++){
-				bw.write(input[i]+",");
-			}
-			bw.write("\n");
-			bw.close();
-		}
-		catch(IOException e){
-			System.out.print(e);
-		}
-		catch(NullPointerException e1){
-			System.out.print("File non trovato\n");
-		}
-	}
-	
-	public int getLineCount(){
-		int i=0;
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while ((br.readLine()) != null){
-				i++;
-			}
-		}
-		catch(IOException e){
-			System.out.print(e);
-		}
-		catch(NullPointerException e1){
-			System.out.print("File non trovato\n");
-		}
-		return i;
-	}
 	
 }
