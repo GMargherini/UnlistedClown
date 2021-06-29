b7e8fcf410f74af0f18f4197e9229794280390ad
GMargherini <84579545+GMargherini@users.noreply.github.com>
2021-06-29 16:05:06 +0200

    Update DataManager.java

--- DataManager.java
+++ DataManager.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FileManager;
 
 import java.io.*;
 import java.util.*;
 
-/**
- *
- * @author dolphin
- */
 public class DataManager {
 	File file;
 	public DataManager(FileManager fm){
